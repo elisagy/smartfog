0f02493de9c01923984dcec6da56aea264684825
Eli Sagy <sagyeli@gmail.com>
2020-01-28 03:27:05 +0200

    Upgrading to Bootstrap 4

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -3,6 +3,7 @@ import {
     ApplicationRef,
 } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import {
     removeNgStyles,
@@ -32,6 +33,7 @@ const appRoutes: Routes = [{ path: '',
 @NgModule({
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         HttpClientModule,
         JwtModule.forRoot({
             config: {
